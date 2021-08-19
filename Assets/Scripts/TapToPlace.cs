b48f26b53df2f0ef9cac05c873c99a5d3790ac44
vadimtsvetkov <vadim.tsvetkov80@gmail.com>
2021-08-19 09:15:34 +0300

    Add assets

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject ObjectToPlace;
+    
+    public ARSessionOrigin ARSessionOrigin;
+    public ARRaycastManager ARRaycastManager;
+
+    private List<ARRaycastHit> _hits; 
+
+    private void Start()
+    {
+        ObjectToPlace.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if(ARRaycastManager.Raycast(Input.GetTouch(0).position, _hits, TrackableType.Planes))
+                ARSessionOrigin.MakeContentAppearAt(ObjectToPlace.transform, _hits[0].pose.position);
+        }
+    }
+}
